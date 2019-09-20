d35bc7d257bb592825fc3556e55245784187612e
Allan Neri Fernandes da Silva <allan.nerifs@gmail.com>
2019-09-20 20:47:47 -0300

    Endpoint cadastrar pedido
    - adicionado rota para cadastrar pedido
    - adicionado métodos uteis na validação de cadastro de pedido em service
    - adicionado configuração para o prettier

--- app/service/pedido_service.js
+++ app/service/pedido_service.js
@@ -1,147 +1,232 @@
 const SqlGenerator = require('sql-generator');
 const sqlgen = new SqlGenerator();
 
-
-module.exports = function (db) {
+module.exports = function(db) {
   return {
-    validarItemPedido(itemPedido) {
-      if (itemPedido === null || itemPedido === undefined){
-        return false
-      }
-      if(!itemPedido.produto || !itemPedido.produto.id_produto){
-        return false
-      }
-  
-      if(!itemPedido.quantidade || itemPedido.quantidade <= 0){
-        return false
-      }
-      
-      if (itemPedido.pizza){
-        if(!itemPedido.sabores || itemPedido.sabores.length === 0) {
-          return false
+    async calcularValorItemPedido({ tamanho, sabores, produto, quantidade }) {
+      let valor = 0;
+      if (tamanho && sabores) {
+        console.info('tamanho e sabores foram informados');
+        const saboresEspeciais = sabores.filter((sabor) => sabor.especial);
+
+        if (saboresEspeciais.length > 0) {
+          console.info('utilizando sabores especiais');
+          saboresParaVerificar = saboresEspeciais;
+        } else {
+          console.info('utilizando sabores tradicionais');
+          saboresParaVerificar = sabores;
         }
-  
-        if(!itemPedido.tamanho || !itemPedido.tamanho.id_tamanho){
-          return false
+
+        const maiorPreco = await this.buscarMaiorPreco(
+          tamanho.id_tamanho,
+          saboresParaVerificar
+        );
+
+        if (!maiorPreco) {
+          console.error(`falha ao encontrar o preço para o item`);
+          return 0;
         }
-  
-        const qtdSabores = itemPedido.sabores.length
-        const qtdMax = itemPedido.tamanho.numero_max_sabores
-        if(qtdSabores > qtdMax) {
-          console.log(qtdSabores,"sabores foram informados, o máximo para esse tamanho é", qtdMax)
-          return false
-        }
-  
+
+        valor = maiorPreco;
+      } else {
+        console.info('produto normal informado');
+        valor = await db
+          .query(
+            `select preco::numeric::float8 from dg.tb_produto where id_produto = $1`,
+            [produto.id_produto]
+          )
+          .then(({ rows }) => rows[0].preco);
       }
-  
-      return true
+
+      return parseFloat(valor);
     },
 
-    async cadastrarPedido(pedido){
-      let idPedido
+    validarItemPedido(itemPedido) {
+      if (!itemPedido) {
+        console.log('item pedido não foi informado');
+        return false;
+      }
+      if (!itemPedido.produto || !itemPedido.produto.id_produto) {
+        console.log('nenhum produto foi informao');
+        return false;
+      }
 
-      await db.query('BEGIN')
+      if (!itemPedido.quantidade || itemPedido.quantidade <= 0) {
+        console.log('quantidade inválida informada');
+        return false;
+      }
 
-      let consulta = sqlgen.insert('dg.tb_pedido',{
+      if (itemPedido.pizza) {
+        if (!itemPedido.sabores || itemPedido.sabores.length === 0) {
+          console.log('sabores da pizza não foram informados');
+          return false;
+        }
+
+        if (!itemPedido.tamanho || !itemPedido.tamanho.id_tamanho) {
+          console.log('tamanho da pizza não foi informado');
+          return false;
+        }
+
+        const qtdSabores = itemPedido.sabores.length;
+        const qtdMax = itemPedido.tamanho.numero_max_sabores;
+        if (!qtdMax) {
+          console.log('informações incompletas');
+          return false;
+        }
+        console.log('quantidade max de sabores no tamanho:', qtdMax);
+        if (qtdSabores > qtdMax) {
+          console.log(
+            qtdSabores,
+            'sabores foram informados, o máximo para esse tamanho é',
+            qtdMax
+          );
+          return false;
+        }
+      }
+      console.log('item valido');
+
+      return true;
+    },
+
+    async cadastrarPedido(pedido) {
+      let idPedido;
+
+      await db.query('BEGIN');
+
+      let consulta = sqlgen.insert('dg.tb_pedido', {
         id_estado: 1,
         id_cliente: pedido.cliente.id_cliente,
         forma_pagamento: pedido.forma_pagamento,
         valor_pago: pedido.valor_pago,
-        total: pedido.total
-        })
-      
-      let query = consulta.sql + ` RETURNING id_pedido`
-        
-      idPedido = await db.query(query,consulta.values)
-      .then( ({rows}) => rows[0].id_pedido)
-      .catch(e => {
-        console.error('falha ao cadastrar pedido', e)
-        return undefined
-      })
-      
-      if (!idPedido) {
-        await db.query('ROLLBACK')
-        return null
-      }
+        valor_desconto: pedido.valor_desconto,
+        subtotal: pedido.subtotal
+      });
+
+      let query = consulta.sql + ` RETURNING id_pedido`;
+
+      // insere na tabela pedido
+      idPedido = await db
+        .query(query, consulta.values)
+        .then(({ rows }) => rows[0].id_pedido)
+        .catch((e) => {
+          db.query('ROLLBACK');
+          console.error('falha ao cadastrar pedido', e);
+          throw 'falha ao cadastrar pedido';
+        });
 
       for (let i = 0; i < pedido.itens.length; i++) {
         const item = pedido.itens[i];
-        
+
         if (item.pizza) {
-          console.log(`${i+1}º item informado é uma pizza`)
-          consulta = sqlgen.insert('dg.tb_item_pedido',{
+          consulta = sqlgen.insert('dg.tb_item_pedido', {
             quantidade: item.quantidade,
             pizza: item.pizza,
             id_produto: item.produto.id_produto,
             id_tamanho: item.tamanho.id_tamanho,
-            id_pedido: idPedido
-          })
-
-        }else {
-          console.log(`${i+1}º item informado é um produto`)
-          consulta = sqlgen.insert('dg.tb_item_pedido',{
+            id_pedido: idPedido,
+            valor: item.valor
+          });
+        } else {
+          consulta = sqlgen.insert('dg.tb_item_pedido', {
             quantidade: item.quantidade,
             pizza: item.pizza,
             id_produto: item.produto.id_produto,
             id_pedido: idPedido
-          })
-        }
-        
-        let query = consulta.sql + ` RETURNING id_item_pedido`
-        
-        let idItemPedido = await db.query(query, consulta.values)
-        .then( ({rows}) => rows[0].id_item_pedido)
-        .catch( e => {
-          console.error(`falha ao cadastrar o ${i+1}º item,${e}` )
-          return false
-        })
-        
-        if(!idItemPedido){
-          db.query('ROLLBACK')
-          return null
+          });
         }
 
-        if(item.pizza){
-          for (let j = 0; j < item.sabores.length; j++) {
-            console.log(`cadastrar ${j+1}º sabor do ${i+1}º item`)
-            const sabor = item.sabores[j];
-            let saborConsulta = sqlgen.insert('dg.tb_item_sabor_pedido',{
-              id_sabor: sabor.id_sabor,
-              id_item_pedido: idItemPedido
-            })
-  
-            const inseriu = await db.query(saborConsulta.sql, saborConsulta.values)
-            .then( s => {
-              console.log(`sabor inserido`)
-              return true
-            })
-            .catch( e => {
-              console.error(`falha ao cadastrar o ${j+1}º sabor. ${e}`)
-              return false
-            })
-  
-            if (!inseriu) {
-              await db.query('ROLLBACK')
-              return null
+        let query = consulta.sql + ` RETURNING id_item_pedido`;
+
+        // insere na tabela item_pedido
+        let idItemPedido = await db
+          .query(query, consulta.values)
+          .then(({ rows }) => rows[0].id_item_pedido)
+          .catch((e) => {
+            db.query('ROLLBACK');
+            console.error(`falha ao cadastrar o ${i + 1}º item,${e}`);
+            throw 'falha ao cadastrar item do pedido';
+          });
+
+        // cadastra os sabores do item pizza
+        if (item.pizza) {
+          let q = `insert into dg.tb_item_sabor_pedido (id_sabor, id_item_pedido) values`;
+          const tamanho = item.sabores.length;
+          let i = 0;
+          do {
+            let sabor = item.sabores[i];
+            let q1 = `(${sabor.id_sabor}, ${idItemPedido})`;
+
+            if (i === 0) {
+              q += ` ${q1}`;
+            } else {
+              q = `${q}, ${q1}`;
             }
-          }
-        }
-        
 
+            i++;
+          } while (i < tamanho);
+
+          const inseriu = await db
+            .query(q)
+            .then((s) => {
+              console.log(`sabores inseridos`);
+              return true;
+            })
+            .catch((e) => {
+              db.query('ROLLBACK');
+              console.error(`falha ao cadastrar os sabores. ${e}`);
+              throw 'falha ao cadastrar sabor';
+            });
+        }
       }
-      
-      await db.query('COMMIT')
-      
-      return this.buscarPedidoCompletoPorID(idPedido)
+
+      await db.query('COMMIT');
+
+      return this.buscarPedidoCompletoPorID(idPedido);
     },
 
-    async buscarPedidoCompletoPorID(id){
+    async buscarPedidoCompletoPorID(id) {
+      const stmt = sqlgen.select('dg.tb_pedido', '*', { id_pedido: id });
 
-      const stmt = sqlgen.select('dg.tb_pedido', '*', {id_pedido: id})
-      
-      return await db.query(stmt.sql, stmt.values).then( ({rows}) => rows[0])
+      return await db.query(stmt.sql, stmt.values).then(({ rows }) => rows[0]);
+    },
+
+    async buscarPrecoDoTamanhoESabor(idTamanho, idSabor) {
+      const consulta = sqlgen.select('dg.tb_sabor_preco_tamanho', ['preco'], {
+        id_tamanho: idTamanho,
+        id_sabor: idSabor
+      });
+
+      return db
+        .query(consulta.sql, consulta.values)
+        .then(({ rows }) => parseFloat(rows.preco))
+        .catch((e) => {
+          console.error(
+            `falha na consulta do preço do sabor no tamanho. Erro: ${e}\nconsulta: ${consulta}`
+          );
+          return null;
+        });
+    },
+
+    async buscarMaiorPreco(idTamanho, sabores) {
+      let ids = new Array();
+      for (let i = 0; i < sabores.length; i++) {
+        const sabor = sabores[i];
+        ids.push(sabor.id_sabor);
+      }
+
+      const consulta = sqlgen.select(
+        'dg.tb_sabor_preco_tamanho',
+        ['MAX(preco)::numeric::float8'],
+        { id_tamanho: idTamanho, id_sabor: { IN: ids } }
+      );
+
+      return db
+        .query(consulta.sql, consulta.values)
+        .then(({ rows }) => rows[0].max)
+        .catch((e) => {
+          console.error(`falha ao realizar consulta dos preços. Erro: ${e}`);
+          return null;
+        });
     }
-
-  }
-  
-}
+  };
+};
