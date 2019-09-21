41f1453a38687919821456f49ea47da1e3d3c951
Allan Neri Fernandes da Silva <allan.nerifs@gmail.com>
2019-09-20 21:06:06 -0300

    Adicionado CORS

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const { Client } = require('pg');
+const cors = require('cors');
 const db = require('./config/db');
 const bodyParser = require('body-parser');
+
+/* ----------------------------------------------------------------- */
+
+const app = express();
+const port = 8080;
 
 const client = new Client({
   connectionString: db.url,
   ssl: true
 });
 
-const app = express();
-const port = 8080;
+/* ----------------------------------------------------------------- */
 
+app.use(cors());
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+/* ----------------------------------------------------------------- */
 
 client
   .connect()
